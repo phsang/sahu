bdaf9c677b994ce7015a68e392a03311d6d7f3bf
phsang <sang.huynhphuoc@mfast.vn>
2024-08-23 13:41:01 +0700

    - Update thành phần sh-avatar

--- projects/sahu/src/lib/sh-avatar/sh-avatar.component.ts
+++ projects/sahu/src/lib/sh-avatar/sh-avatar.component.ts
@@ -1,22 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, ElementRef, Renderer2, AfterViewInit, input } from '@angular/core';
 
 @Component({
   selector: 'sh-avatar',
   templateUrl: './sh-avatar.component.html',
   styleUrls: ['./sh-avatar.component.scss']
 })
-export class ShAvatarComponent implements OnInit {
+export class ShAvatarComponent implements AfterViewInit {
   @Input() shSrc?: string;
   @Input() shClass?: string;
   @Input() shIcon?: string;
   @Input() shText?: string;
+  @Input() shAnimated: true | false = true;
   @Input() shShape: 'circle' | 'square' = 'circle';
   @Input() shSize?: 'large' | 'small' | number;
   @Input() shOnload?: () => void;
 
   avatarClass = '';
 
-  ngOnInit(): void {
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
     this.avatarClass = `${this.shClass ? this.shClass : ''} sh-avatar-${this.shShape}`;
 
     if (this.shSize === 'large') {
@@ -24,12 +27,17 @@ export class ShAvatarComponent implements OnInit {
     } else if (this.shSize === 'small') {
       this.avatarClass += ' sh-avatar-small';
     }
+
+    if (this.shAnimated) {
+      this.avatarClass += ' sh-avatar-animated';
+    }
   }
 
   handleLoad(): void {
     if (this.shOnload) {
       this.shOnload();
     }
+    this.renderer.addClass(this.el.nativeElement, 'avatar-loaded');
   }
 
   isNumeric(value: any): value is number {
