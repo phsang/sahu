77c67065ecbd4137bde3d5fcfbb5833b95810e59
phsang <sang.huynhphuoc@mfast.vn>
2024-09-06 14:30:25 +0700

    - thành phần sh-modal

--- projects/sahu/src/lib/sh-modal/sh-modal.component.ts
+++ projects/sahu/src/lib/sh-modal/sh-modal.component.ts
@@ -1,21 +1,34 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
+import { ShButtonComponent } from '../sh-button/sh-button.component';
 
 @Component({
   selector: 'sh-modal',
+  imports: [ShButtonComponent],
   templateUrl: './sh-modal.component.html',
   styleUrls: ['./sh-modal.component.scss']
 })
 export class ShModalComponent {
-  @Input() shRouterLink?: string = '';
-  @Input() shType?: 'modal' | 'submit' | 'reset' = 'modal';
-  @Input() shClass: string = '';
-  @Input() disabled = false;
+  @Input() shIcon?: string;
+  iconElement?: SafeHtml = '';
+  @Input() shType?: 'success' | 'warning' | 'error';
+  @Input() shTitle?: string;
+  @Input() shMdSize?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full' = 'md';
+  @Input() shMdClass?: string;
+  @Input() shHeadClass?: string;
+  @Input() shBodyClass?: string;
+  @Input() shFootClass?: string;
+  @Input() shHead?: boolean;
+  @Input() shFoot?: boolean;
+  @Input() shCanClose?: boolean;
+  @Output() shCancel = new EventEmitter<Event>();
+  @Output() shOk = new EventEmitter<Event>();
 
-  @Output() shClick = new EventEmitter<Event>();
+  handleCancel(event: Event): void {
+    this.shCancel.emit(event);
+  }
 
-  handleClick(event: Event): void {
-    if (!this.disabled) {
-      this.shClick.emit(event);
-    }
+  handleOk(event: Event): void {
+    this.shOk.emit(event);
   }
 }
