0e5a0e0b1735a37d63f8ac4a0bd87c5d748bf393
phsang <sang.huynhphuoc@mfast.vn>
2025-03-04 09:54:40 +0700

    date-picker

--- projects/sahu/src/lib/sh-date-picker/sh-date-picker.component.ts
+++ projects/sahu/src/lib/sh-date-picker/sh-date-picker.component.ts
@@ -52,7 +52,6 @@ export class ShDatePickerComponent implements OnInit, ControlValueAccessor, OnCh
 
   inputValue: any = null;
   dropdownPosition: string = 'drop_down';
-  selectedOptions: any[] = [];
   datePickerOpen = false;
 
   // ControlValueAccessor methods
@@ -61,22 +60,12 @@ export class ShDatePickerComponent implements OnInit, ControlValueAccessor, OnCh
 
   ngOnInit(): void {
     this.initializeOptions();
-    this.selectedOptions = [];
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['shData'] && !changes['shData'].firstChange) {
-      this.initializeOptions();
-      this.updateSelectedOptions();
-    }
   }
 
   updateSelectedOptions(): void {
-    if (this.inputValue) {
-      this.selectedOptions = this.shMultiple
-        ? this.shData.filter(option => this.inputValue.includes(option.value))
-        : [this.shData.find(option => option.value === this.inputValue)];
-    }
   }
 
   initializeOptions(): void {
@@ -111,61 +100,28 @@ export class ShDatePickerComponent implements OnInit, ControlValueAccessor, OnCh
 
   modelChangeEmit() {
     let _model: any[] = [];
-    if (this.shMultiple) {
-      this.selectedOptions.forEach(option => {
-        _model.push(option.value);
-      });
-    } else {
-      _model = this.selectedOptions[0]?.value;
-    }
     this.inputValue = _model;
     this.onChange(_model);
     this.shChange.emit(_model); // Emit the model change to the parent
   }
 
   selectOption(option: any, event: any): void {
-    if (this.shMultiple) {
-      if (event.target.checked) {
-        this.selectedOptions.push(option);
-      } else {
-        this.selectedOptions = this.selectedOptions.filter(o => o !== option);
-      }
-    } else {
-      this.selectedOptions = [option];
-      this.toggleDropdown();
-    }
     this.modelChangeEmit();
   }
 
   removeTag(option: any, event: MouseEvent): void {
     event.stopPropagation();
-    this.selectedOptions = this.selectedOptions.filter(o => o !== option);
-    this.modelChangeEmit();
   }
 
   reset(event: Event): void {
     event.stopPropagation();
-    this.selectedOptions = [];
     this.modelChangeEmit();
   }
 
   // ControlValueAccessor methods
   writeValue(value: any): void {
     if (value) {
-      let _model: any[] = [];
-      this.selectedOptions = this.shMultiple ? this.shData.filter(option => value.includes(option.value)) : [this.shData.find(option => option.value === value)];
-
-      if (this.shMultiple) {
-        this.selectedOptions.forEach(option => {
-          _model.push(option.value);
-        });
-      } else {
-        _model = this.selectedOptions[0]?.value;
-      }
-      this.inputValue = _model;
     } else {
-      this.selectedOptions = [];
-      this.inputValue = null;
     }
   }
 
@@ -180,10 +136,4 @@ export class ShDatePickerComponent implements OnInit, ControlValueAccessor, OnCh
   setDisabledState?(isDisabled: boolean): void {
     // Add logic to manage the disabled state if needed
   }
-
-  removeAccent(str: string) {
-    return str.normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .replace(/đ/g, 'd').replace(/Đ/g, 'D');
-  };
 }
