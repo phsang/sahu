cf91019d2f8e750a8c124a72ee176ae3812ad76d
phsang <sang.huynhphuoc@mfast.vn>
2025-03-06 13:31:29 +0700

    date-picker

--- projects/sahu/src/lib/sh-date-picker/date-picker-panel/sh-date-picker-panel.module.ts
+++ projects/sahu/src/lib/sh-date-picker/date-picker-panel/sh-date-picker-panel.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DatePickerPanelComponent } from './sh-date-picker-panel.component';
 import { FormsModule } from '@angular/forms';
 import { CalendarBoxComponent } from '../calendar-box/sh-calendar-box.component';
+import { ShButtonModule } from '../../sh-button/sh-button.module';
 
 @NgModule({
   declarations: [DatePickerPanelComponent, CalendarBoxComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ShButtonModule],
   exports: [DatePickerPanelComponent, CalendarBoxComponent],
 })
 export class ShDatePickerPanelModule { }
