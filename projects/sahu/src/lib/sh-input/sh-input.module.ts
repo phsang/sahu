3409751bf29fd79f77971066ceb0c2e919320fcf
phsang <sang.huynhphuoc@mfast.vn>
2025-03-10 11:01:21 +0700

    input

--- projects/sahu/src/lib/sh-input/sh-input.module.ts
+++ projects/sahu/src/lib/sh-input/sh-input.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShInputComponent } from './sh-input.component';
 import { ShButtonModule } from '../sh-button';
+import { ShIconModule } from '../sh-icon';
 
 @NgModule({
   declarations: [ShInputComponent],
-  imports: [CommonModule, ShButtonModule],
+  imports: [
+    CommonModule,
+    ShIconModule,
+    ShButtonModule,
+  ],
   exports: [ShInputComponent],
 })
 export class ShInputModule { }
