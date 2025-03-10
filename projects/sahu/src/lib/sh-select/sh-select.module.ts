e4608eadf07fa4973b22c59a62adae37ee6fd497
phsang <sang.huynhphuoc@mfast.vn>
2025-03-10 16:42:12 +0700

    select

--- projects/sahu/src/lib/sh-select/sh-select.module.ts
+++ projects/sahu/src/lib/sh-select/sh-select.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShSelectComponent } from './sh-select.component';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ShButtonModule } from '../sh-button';
+import { ShSelectComponent } from './sh-select.component';
 
 @NgModule({
   declarations: [ShSelectComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    ShButtonModule,
+  ],
   exports: [ShSelectComponent],
 })
 export class ShSelectModule { }
