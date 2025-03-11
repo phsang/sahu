78c90b07f00d79b03bb34f7a3f945a6f400e6739
phsang <sang.huynhphuoc@mfast.vn>
2025-03-11 10:14:03 +0700

    pagination

--- projects/sahu/src/lib/sh-pagination/sh-pagination.module.ts
+++ projects/sahu/src/lib/sh-pagination/sh-pagination.module.ts
@@ -4,6 +4,7 @@ import { ShPaginationComponent } from './sh-pagination.component';
 import { RouterModule } from '@angular/router';
 import { ShIconModule } from '../sh-icon/sh-icon.module';
 import { FormsModule } from '@angular/forms';
+import { ShSelectModule } from '../sh-select';
 
 @NgModule({
   declarations: [ShPaginationComponent],
@@ -12,6 +13,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     RouterModule,
     ShIconModule,
+    ShSelectModule,
   ],
   exports: [ShPaginationComponent],
 })
